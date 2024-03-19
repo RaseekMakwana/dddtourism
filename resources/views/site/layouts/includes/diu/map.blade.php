3d633b64bbab6111bd7dfc1c14641010fb2e2523
rasik.makwana <makwanarasikbhai.prabhubhai@dainikbhaskar.com>
2024-03-19 08:22:18 +0530

    df

---
+++ resources/views/site/layouts/includes/diu/map.blade.php
@@ -0,0 +1,49 @@
+<section class="infomain">
+    <div class="container">
+        <div class="row ">
+			<div class="col-lg-11 ml-auto col-md-11 col-12">
+                <div class="row bg-black-op">
+                    <div class="col-lg-6 pr-lg-0 col-md-6 col-12">
+                        <div class="row">
+                            <div class="col-lg-12 iframe col-md-12 col-12">
+                                <img src="{{ asset('assets/site/img/map-diu.jpg') }}">
+                            </div>
+                            <div class="col-lg-10 text-center col-md-10 col-12">
+                                <p><a href="#">Click Here For Direction</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12">
+                        <div class="section-title white">
+                            <h2>Diu</h2>
+                            <h6>A Perfect Getaway for Friends & Family</h6>
+                            <p class="mb-4">Diu is a beautiful island located nearby the coast of kathiyawad region at veraval port. Diu is more popular as a tourist place because of the natural beaches, Bold History, many other attractions and sightseeing.</p>
+                            <p>Area:40.00 Sq. Km</p>
+                            <p>Population: 52,074 (As per 2011 Census)</p>
+                            <p>Languages Spoken: Gujarati, Marathi, Portuguese, Hindi, and Others</p>
+                            <p>Best Time to Visit: Monsoon & Winter</p>
+                        </div>
+                        <div class="inf">
+                            <div class="box">
+                                <div class="img"><img src="{{ asset('assets/site/img/icr-1.png') }}" alt="" /></div>
+                                <div class="text">By Road</div>
+                            </div>
+                            <div class="box red">
+                                <div class="img"><img src="{{ asset('assets/site/img/icr-2.png') }}" alt="" /></div>
+                                <div class="text">By Train</div>
+                            </div>
+                            <div class="box blue">
+                                <div class="img"><img src="{{ asset('assets/site/img/icr-3.png') }}" alt="" /></div>
+                                <div class="text">By Air</div>
+                            </div>
+                            <div class="box green">
+                                <div class="img"><img src="{{ asset('assets/site/img/icr-4.png') }}" alt="" /></div>
+                                <div class="text">By chopper</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
